ee07801d110bd6af0e678a52b116cf09d7049336
kostiack <kostia.ck@gmail.com>
2016-10-30 21:13:49 +0200

    1

--- lesson1.rb
+++ lesson1.rb
@@ -1,14 +1,32 @@
 require 'date'
+require 'time'
+
 class Lesson1
   def sum(val = 0)
-    # TODO
+    val.split('').map(&:to_i).inject(:+)
   end
 
   def age(birthday)
-    # TODO
+    # '1986-10-29'
+    time_now = Time.now
+    time_birtday = Time.new(birthday.year, birthday.month, birthday.day)
+
+    seconds = (time_now - time_birtday).to_i
+    minutes = seconds / 60
+    hours  = minutes / 60
+    days = hours / 24
+    years = time_now.year - time_birtday.year
+
+    puts "Я живу #{years} года или #{days} дней или #{hours} часов или #{minutes} минут или #{seconds} секунд"
+
   end
 
+
   def name
-    # TODO
+    puts "Please enter your name:"
+    get_name = gets.chop
+    str = "Hello #{get_name} Super Man!"
+    puts str
+    str
   end
 end
--- my_array.rb
+++ my_array.rb
@@ -1,63 +1,67 @@
 class MyArray
-  attr_accessor :array
+  attr_reader :array
 
   def initialize(arr = [])
+    # Конструктор який приймає масив і зберігає його в змінну (initialize)
     @array = arr
+    # @array = Array.new(6)
+    # @array = [1, 5, 6, 2, 6, 3]
   end
 
-  def size
-    # TODO
+  def size # Метод який повертає розмір масива
+    @array.size
   end
 
-  def reverse
-    # TODO
+  def reverse # Метод який повертає перевернутий масив
+    @array.reverse
   end
 
-  def max
-    # TODO
+  def max # Метод який повертає найбільший елемент масива
+    @array.max
   end
 
-  def min
-    # TODO
+  def min # Метод який повертає найменший елемент масива
+    @array.min
   end
 
-  def desc
-    # TODO
+  def asc # Відсортований по зростанню (asc)
+    @array.sort
   end
 
-  def asc
-    # TODO
+  def desc # Відсортований по спаданню (desc)
+    @array.sort.reverse
   end
 
-  def odd
-    # TODO
+  def odd # Метод який повертає лише непарні числа
+    @array.find_all { |i| (i % 2).nonzero? }
   end
 
-  def multiple_to_three
-    # TODO
+  def multiple_to_three # Метод який повертає лише числа кратні трьом
+    @array.find_all	{ |i| (i % 3).zero? }
   end
 
-  def uniq
-    # TODO
+  def uniq # Метод який повертає лише унікальні числа
+    @array.uniq
   end
 
-  def devide_on_ten
-    # TODO
+  def devide_on_ten  # Метод який повертає масив елементи якого розділені на 10 зі знаком після коми
+    @array.map { |i| i / 10.0 }
   end
 
-  def chars
-    # TODO
+  def chars # Метод який повертає масив з символами алфавіту відповідно до індексу елементів масиву (chars)
+    chars = (:a..:z).to_a
+    @array.map { |i| chars[i]}
   end
 
-  def switch
-    # TODO
+  def switch # Метод який повертає масив у якому максимальний та мінімальний елементи поміняні місцями (switch)
+    # :TODO
   end
 
-  def before_min
-    # TODO
+  def before_min # Метод який повертає масив, який містить елементи, що передують найменшому елементу
+    @array[0...@array.index(@array.min)]
   end
 
-  def three_smallest
-    # TODO
+  def three_smallest # Метод який повертає масив, який містить 3 найменші елементи
+    @array.sort[0..2]
   end
 end
